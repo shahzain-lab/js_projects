3a83b786419ae6df584ea88abec7126686518e53
shahzain <69709547+shahzain-lab@users.noreply.github.com>
2021-02-03 02:11:08 -0800

    api passed

--- SearchApp/js/main.js
+++ SearchApp/js/main.js
@@ -1,3 +1,6 @@
+import { setSearchFocus } from './searchBar.js';
+import { searchTerm } from './dataFunction.js';
+ 
 document.addEventListener('reactdystatechange',(e) => {
     if(e.target.raedyState === 'complete') {
         initApp();
@@ -5,8 +8,28 @@ document.addEventListener('reactdystatechange',(e) => {
 });
 
 const initApp =()=> {
-    //set the focus
+    setSearchFocus()
 
 
     //3 listners
+   const form = doucment.querySelector('#searchBar');
+   form.addEventListener('submit' ,submitSearch);
+
+};
+
+//procedural 'workflow' function
+const submitSearch =(e) => {
+    e.preventDefault()
+
+    //delete search results
+    processTheSearch();
+
+    setSearchFocus()
+}
+
+const processTheSearch = async () => {
+    //clear stats
+    const searchTerm = getSearchTerm()
+    if(searchTerm === "") return;
+    const resultArray = await retrieveSearchResults(searchTerm)
 }
