a14d07d8ab5ed03b41eb55b60640e7c0de7c6708
shahzain <69709547+shahzain-lab@users.noreply.github.com>
2021-02-03 01:16:31 -0800

    js modules

---
+++ SearchApp/js/main.js
@@ -0,0 +1,12 @@
+document.addEventListener('reactdystatechange',(e) => {
+    if(e.target.raedyState === 'complete') {
+        initApp();
+    }
+});
+
+const initApp =()=> {
+    //set the focus
+
+
+    //3 listners
+}
