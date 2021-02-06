101bfc303ca44516ff3349876ff0566bb7e4020b
shahzain <69709547+shahzain-lab@users.noreply.github.com>
2021-02-06 14:16:19 -0800

    completed counter app

--- CounterApp/app.js
+++ CounterApp/app.js
@@ -1,12 +1,36 @@
-const value = document.getElementsByClassName("value");
+let colors = [1 ,2, 3 , 4, 5, 6 , 7, 8, 9 ,"A" , "B", "C" ,"D" ,"E", "F"];
+
+const value = document.querySelector(".value");
 const btns = document.querySelectorAll(".btn");
+const counter = document.getElementById('counter')
+
 
 let count = 0;
 
 btns.forEach((btn) => {
-    let values = btn.classList;
+    btn.addEventListener('click', (e) => {
+        const classVal = e.currentTarget.classList;
 
-    if(values.contains('decrease')){
+        if(classVal.contains('decrease')){
+            count--
+        }
+        else if(classVal.contains('increase')){
+            count++
+        }
+        else{
+            count = 0;
+        };
+        value.textContent = count;
 
-    }
-})
+        hexHash = "#";
+        for(let i = 0; i < 6; i++ ){
+            hexHash += colors[getColor()]
+        }
+
+        counter.style.color = hexHash
+    })
+});
+
+function getColor(){
+    return Math.floor(Math.random() * colors.length)
+}
