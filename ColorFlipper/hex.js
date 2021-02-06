2508a9933bc890067f340881d1ad3386f290ca3b
shahzain <69709547+shahzain-lab@users.noreply.github.com>
2021-02-06 12:21:49 -0800

    completed color flipper

---
+++ ColorFlipper/hex.js
@@ -0,0 +1,19 @@
+let colors = [1 ,2, 3 , 4, 5, 6 , 7, 8, 9 ,"A" , "B", "C" ,"D" ,"E", "F"];
+
+const btn = document.querySelector('#btn');
+const colrName = document.querySelector('#color');
+
+btn.addEventListener('click', () => {
+    let hexLoop = "#";
+
+    for(let i= 0 ; i < 6; i++ ){
+        hexLoop += colors[getRandomNumber()]
+    }
+    console.log(hexLoop)
+    document.body.style.backgroundColor = hexLoop;
+    colrName.textContent = hexLoop;
+});
+
+function getRandomNumber(){
+    return Math.floor(Math.random() * colors.length);
+}
