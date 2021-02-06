a3e0604da7daab591cd50badbe4d8fe18cba3a1d
shahzain <69709547+shahzain-lab@users.noreply.github.com>
2021-02-06 13:38:52 -0800

    functioning

---
+++ CounterApp/app.js
@@ -0,0 +1,12 @@
+const value = document.getElementsByClassName("value");
+const btns = document.querySelectorAll(".btn");
+
+let count = 0;
+
+btns.forEach((btn) => {
+    let values = btn.classList;
+
+    if(values.contains('decrease')){
+
+    }
+})
