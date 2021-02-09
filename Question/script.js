c5e213097a69a8a2945f63010f9562b773822a40
shahzain <69709547+shahzain-lab@users.noreply.github.com>
2021-02-09 11:31:06 -0800

    toggle showed

---
+++ Question/script.js
@@ -0,0 +1,11 @@
+
+
+const questions = document.querySelectorAll('.question');
+
+questions.forEach((question) => {
+    const btn = question.querySelector('.question-btn');
+
+    btn.addEventListener('click', () => {
+        question.classList.toggle('show-text')
+    })
+})
