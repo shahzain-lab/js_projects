f46c9523b3c57ea84e4e4bf6d29821c87c92dd7d
shahzain <69709547+shahzain-lab@users.noreply.github.com>
2021-02-09 11:37:21 -0800

    complete Q/A

--- Question/script.js
+++ Question/script.js
@@ -6,6 +6,11 @@ questions.forEach((question) => {
     const btn = question.querySelector('.question-btn');
 
     btn.addEventListener('click', () => {
+        questions.forEach((item) => {
+             if(item !== question){
+                 item.classList.remove('show-text')
+             }
+        })
         question.classList.toggle('show-text')
     })
 })
