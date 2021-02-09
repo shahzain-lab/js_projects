b8702c5707b3c407e8f7096ba4c8dc4c8fb321d0
shahzain <69709547+shahzain-lab@users.noreply.github.com>
2021-02-09 08:45:00 -0800

    modal pro done

--- Model/script.js
+++ Model/script.js
@@ -1,1 +1,12 @@
+
 const modalBtn = document.querySelector('.modal-btn');
+const showModal = document.querySelector(".modal-overlay");
+const closeBtn = document.querySelector('.close-btn');
+
+modalBtn.addEventListener('click', () => {
+    showModal.classList.toggle('open-modal');
+});
+
+closeBtn.addEventListener('click', () => {
+    showModal.classList.remove('open-modal')
+})
