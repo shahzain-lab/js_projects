8d0e49472f0d4251260ca2653fe9eca14165fb68
shahzain <69709547+shahzain-lab@users.noreply.github.com>
2021-02-12 13:11:24 -0800

    styled

---
+++ menu/app.js
@@ -0,0 +1,59 @@
+const menu = [
+    {
+        title:'charred-lemon-chicken-piccata-recipe',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.Autem aliquid ipsum quaerat error vero reiciendis, sequi numquam labore.',
+        img:'./img/charred-lemon-chicken-piccata-recipe.webp',
+    },
+    {
+        title:'chicken-diavola-with-polenta-and-zucchini-flowers',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.Autem aliquid ipsum quaerat error vero reiciendis, sequi numquam labore.',
+        img:'./img/chicken-diavola-with-polenta-and-zucchini-flowers.jpg',
+    },
+    {
+        title:'chicken-masala-recipe',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.Autem aliquid ipsum quaerat error vero reiciendis, sequi numquam labore.',
+        img:'./img/chicken-masala-recipe.jpg',
+    },
+    {
+        title:'KitKat-ice-cream-pie',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.Autem aliquid ipsum quaerat error vero reiciendis, sequi numquam labore.',
+        img:'./img/KitKat-ice-cream-pie.jpg',
+    },
+    {
+        title:'maple-thyme-roast-chicken',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.Autem aliquid ipsum quaerat error vero reiciendis, sequi numquam labore.',
+        img:'./img/maple-thyme-roast-chicken.jpg',
+    },
+    {
+        title:'sauted-vegetable-medley',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.Autem aliquid ipsum quaerat error vero reiciendis, sequi numquam labore.',
+        img:'./img/sauted-vegetable-medley.jpeg',
+    },
+    {
+        title:'Scoops-kinds-ice-cream',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.Autem aliquid ipsum quaerat error vero reiciendis, sequi numquam labore.',
+        img:'./img/Scoops-kinds-ice-cream.jpg',
+    },
+    {
+        title:'skinny-broccoli-and-mixed-vege-dish',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.Autem aliquid ipsum quaerat error vero reiciendis, sequi numquam labore.',
+        img:'./img/skinny-broccoli-and-mixed-vege-dish.jpg',
+    },
+    {
+        title:'Tequila-Chicken-Recipe',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.Autem aliquid ipsum quaerat error vero reiciendis, sequi numquam labore.',
+        img:'./img/Tequila-Chicken-Recipe.jpg',
+    },
+    {
+        title:'UbeIce Cream HERO',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.Autem aliquid ipsum quaerat error vero reiciendis, sequi numquam labore.',
+        img:'./img/UbeIceCreamHERO.jpg',
+    },
+    {
+        title:'vegetable-stir-fry.jpg',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.Autem aliquid ipsum quaerat error vero reiciendis, sequi numquam labore.',
+        img:'./img/vegetable-stir-fry.jpg',
+    },
+];
+
+console.log(menu)
