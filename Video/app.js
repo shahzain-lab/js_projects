38abad221f1d95338a2c556a703cfedca169c701
shahzain <69709547+shahzain-lab@users.noreply.github.com>
2021-02-15 11:25:33 -0800

    js dom

--- Video/app.js
+++ Video/app.js
@@ -1,3 +1,12 @@
 
 
-const videoContainer = document.querySelector('.video-container')
+const video = document.querySelector('.video-container');
+const slidingBtn = document.querySelector('.switch-btn');
+
+slidingBtn.addEventListener('click', () => {
+    if(switched.classList.contains('switch')){
+        switched.classList.add('slide');
+        video.pause();
+    }
+    
+})
