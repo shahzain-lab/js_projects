7ad88214f0a489c0b826d5decb548dafa8db3340
shahzain <69709547+shahzain-lab@users.noreply.github.com>
2021-02-15 11:36:56 -0800

    preloader added

--- Video/app.js
+++ Video/app.js
@@ -4,9 +4,18 @@ const video = document.querySelector('.video-container');
 const slidingBtn = document.querySelector('.switch-btn');
 
 slidingBtn.addEventListener('click', () => {
-    if(switched.classList.contains('switch')){
-        switched.classList.add('slide');
-        video.pause();
-    }
-    
+  if(!slidingBtn.classList.contains('slide')){
+      slidingBtn.classList.add('slide');
+      video.pause();
+  }else{
+      slidingBtn.classList.remove('slide')
+      video.play();
+  }
+});
+
+//preloader
+const preloader = document.querySelector('.preloader');
+
+window.addEventListener('load', () => {
+    preloader.classList.add('hide-preloader');
 })
