e14858364d92cddc4ca3b11856783c17dd3fdeb0
shahzain <69709547+shahzain-lab@users.noreply.github.com>
2021-02-25 09:45:37 -0800

    update input feild

--- tracker app/app.js
+++ tracker app/app.js
@@ -4,17 +4,29 @@
 const submit = document.getElementById('submit');
 const desc = document.getElementById('desc');
 const amount = document.getElementById('amount');
+const required = document.getElementById('required');
 
  
 submit.addEventListener('click', (e) => {
     e.preventDefault();
     
-    if(desc.value === ''){
-        console.log("dk");
+    if(desc.value === '' || amount.value === ''){
+        const req = document.createElement('p');
+        req.classList.add('required');
+        req.textContent = 'Feild is Required';
+        required.append(req)
+        setTimeout(() => {
+            req.remove()
+        }, 3000);
     }else{
-        console.log("kjk");
+        desc.value = '';
+        amount.value = '';
+        getData();
     }
     
-   
+});
+
+
+function getData(){
     
-})
+}
